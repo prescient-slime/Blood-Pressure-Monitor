6936b456a194a5addcb446fd6818b596787bc769
Slime-Sama <prescient.slime@gmail.com>
2022-05-08 18:59:36 -0500

    Imported OpenCV successfully

--- app/src/main/java/com/example/bloodpressuremonitor/MainActivity.java
+++ app/src/main/java/com/example/bloodpressuremonitor/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.bloodpressuremonitor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static String LOGTAG = "OpenCV_Log";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (OpenCVLoader.initDebug()){
+            Log.d(LOGTAG, "OpenCV Initialized Successfully");
+        }
     }
 }
