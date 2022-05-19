6037c5e73df43eaad83fea7db09983d9bb16a27e
Slime-Sama <prescient.slime@gmail.com>
2022-05-19 13:23:48 -0500

    Surfaceview Created successfully. Now need physical device for testing and frame processing.

--- app/src/main/java/com/example/bloodpressuremonitor/MainActivity.java
+++ app/src/main/java/com/example/bloodpressuremonitor/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends CameraActivity {
+
+
     private static String LOGTAG = "OpenCV_Log";
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case LoaderCallbackInterface.SUCCESS:{
+                    Log.v(LOGTAG, "OpenCV Loaded");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:{
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +45,62 @@ public class MainActivity extends AppCompatActivity {
         if (OpenCVLoader.initDebug()){
             Log.d(LOGTAG, "OpenCV Initialized Successfully");
         }
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.opencv_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(cvCameraViewListener);
+
     }
+
+    @Override
+    protected List<?extends CameraBridgeViewBase> getCameraViewList(){
+        return Collections.singletonList(mOpenCvCameraView);
+    }
+
+    private CameraBridgeViewBase.CvCameraViewListener2 cvCameraViewListener = new CameraBridgeViewBase.CvCameraViewListener2(){
+
+        @Override
+        public void onCameraViewStarted(int width, int height) {
+
+        }
+
+        @Override
+        public void onCameraViewStopped() {
+
+        }
+
+        @Override
+        public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+            return inputFrame.rgba();
+        }
+    };
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(LOGTAG, "OpenCV Not Found. Initializing... ");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+
+        } else {
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
+        }
+    }
+
 }
